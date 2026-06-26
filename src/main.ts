2ac0734158521af5a267309b4a570bb5cc1321f4
thangvu123321 <vu713194@gmail.com>
2026-06-26 09:22:54 +0700

    cập nhật logo lan 2

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,13 @@ console.log('🔥 Angular đang khởi động... Splash screen sẽ hiển th�
 
 bootstrapApplication(App, appConfig)
   .then(() => {
-    console.log('✅ Angular khởi động xong, ẩn splash screen');
     const splash = document.getElementById('splash-screen');
     if (splash) {
       splash.classList.add('opacity-0');
       setTimeout(() => {
         splash.remove();
-        console.log('🧹 Splash screen đã bị gỡ khỏi DOM');
       }, 500);
     } else {
-      console.warn('⚠️ Không tìm thấy #splash-screen trong DOM');
     }
   })
   .catch((err) => console.error('❌ Lỗi khi bootstrap Angular:', err));
